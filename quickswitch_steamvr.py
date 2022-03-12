e6977126f35507a8dbef0ddbbec1f9a46a3304b9
aspitarl <aspitarte@gmail.com>
2022-03-11 18:00:24 -0800

    add argparse

--- quickswitch_steamvr.py
+++ quickswitch_steamvr.py
@@ -1,26 +1,24 @@
 import shutil
 import sys
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("option", type=str, default='normal', choices=['normal','seated','no_hmd'], 
+    help="Which controller")
+
+
+option = parser.parse_args().option
+
+print("Switching to option: {}".format(option))
 
 config_folders_path = r'VR Config Shortcuts'
-
-# config_folders = {
-#     'no_hmd': os.path.join(config_folders_path, 'no_hmd'),
-#     'normal': os.path.join(config_folders_path, 'normal'),
-#     'normal_nopower': os.path.join(config_folders_path, 'normal_nopower'),
-# }
-
-# config_folder = config_folders[option]
 
 steamvr_settings_out = r'C:\Program Files (x86)\Steam\config\steamvr.vrsettings'
 null_driver_out = r'C:\Program Files (x86)\Steam\steamapps\common\SteamVR\drivers\null\resources\settings\default.vrsettings'
 
 #Run normal_nopower to keep base stations on then switch to no_hmd...not working
-# 
-option = 'normal'
-# option = 'seated'
 # option = 'normal_nopower' #Can't gt to work. Seems have to switch power settins in steamvr and nohmd cannot wake base stations.
-# option = 'no_hmd'
 
 config_folder = os.path.join(config_folders_path, option)
 
